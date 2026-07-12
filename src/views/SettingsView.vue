796a9a18c8adef3fec5a1407b949cfb96b6ef7e7
shadaileng <qpf0510@qq.com>
2026-07-12 15:52:22 +0800

    refactor: 移除配置页保存/清除按钮，UID/Token 设为只读

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -4,7 +4,7 @@ import { useRouter } from 'vue-router'
 import { useSettingsStore } from '@/stores/settings'
 import { useUserStore } from '@/stores/user'
 import { useMirrorConfig } from '@/composables/useMirrorConfig'
-import { showToast, showSuccessToast, showDialog as showConfirmDialog } from 'vant'
+import { showSuccessToast, showDialog as showConfirmDialog } from 'vant'
 import UserInfo from '@/components/UserInfo.vue'
 
 const router = useRouter()
@@ -12,13 +12,9 @@ const settings = useSettingsStore()
 const userStore = useUserStore()
 const { showDialog, mirrorUrl, mirrorDisplay, openConfig, saveConfig } = useMirrorConfig()
 
-const uid = ref(settings.uid || '')
-const token = ref(settings.token || '')
 const currentUser = ref<any>(userStore.current)
 
 onMounted(async () => {
-  uid.value = settings.uid
-  token.value = settings.token
   if (settings.isLoggedIn) {
     await loadCurrentUser()
   }
@@ -27,28 +23,6 @@ onMounted(async () => {
 const loadCurrentUser = async () => {
   const data = await userStore.fetchCurrent()
   if (data) currentUser.value = data
-}
-
-function save() {
-  if (!uid.value) {
-    showToast('UID不能为空')
-    return
-  }
-  if (!token.value) {
-    showToast('Token不能为空')
-    return
-  }
-  settings.setCredentials(uid.value, token.value)
-  showSuccessToast('保存成功')
-  loadCurrentUser()
-}
-
-function clear() {
-  uid.value = ''
-  token.value = ''
-  userStore.logout()
-  currentUser.value = null
-  showSuccessToast('已清除')
 }
 
 function handleLogout() {
@@ -60,8 +34,6 @@ function handleLogout() {
     .then(() => {
       userStore.logout()
       currentUser.value = null
-      uid.value = ''
-      token.value = ''
       showSuccessToast('已退出')
     })
     .catch(() => {})
@@ -89,16 +61,13 @@ function handleLogout() {
 
     <van-cell-group inset class="setting-group">
       <van-cell title="认证配置" />
-      <van-field v-model="uid" label="UID" placeholder="请输入UID" clearable />
-      <van-field v-model="token" label="Token" placeholder="请输入Token" type="password" clearable />
+      <van-field :model-value="settings.uid" label="UID" readonly />
+      <van-field :model-value="settings.token" label="Token" type="password" readonly />
     </van-cell-group>
 
     <van-cell-group inset class="mirror-group">
       <van-cell title="镜像源（数据源地址）" :value="mirrorDisplay()" is-link @click="openConfig" />
     </van-cell-group>
-
-    <van-button type="primary" block round class="save-btn" @click="save">保存</van-button>
-    <van-button type="warning" block round class="clear-btn" @click="clear">清除</van-button>
 
     <van-cell-group inset class="info-group">
       <van-cell title="数据来源">
@@ -133,12 +102,6 @@ function handleLogout() {
   margin-top: 4px;
   color: #ee0a24;
   font-size: 12px;
-}
-.save-btn {
-  margin: 16px 12px;
-}
-.clear-btn {
-  margin: 0 12px 16px;
 }
 .info-group {
   margin: 12px;
